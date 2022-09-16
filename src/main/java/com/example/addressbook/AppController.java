3ceb2ef233169b0a37cfee89884736af52a42cb5
hakareem <harithakareem72@gmail.com>
2022-09-16 10:01:04 +0100

    routes

--- src/main/java/com/example/addressbook/AppController.java
+++ src/main/java/com/example/addressbook/AppController.java
@@ -1,5 +1,9 @@
 package com.example.addressbook;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AppController {
 
+ @Autowired 
+ private AddressService addressService;
+
+ @GetMapping("/addresses")
+ public List<Address> getAll() {
+  return addressService.getAll();
+ }
+
  @PostMapping("/addresses")
  public Address saveData(@RequestBody Address address) {
-  return address;
+  return addressService.save(address);
  }
 }
